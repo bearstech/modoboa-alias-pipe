6c099b61f518a7effe7521d7d7c03874715880d6
Stéphane Klein <contact@stephane-klein.info>
2015-05-22 17:01:22 +0200

    Append postfix_maps command to generate postfix config files

--- README.rst
+++ README.rst
@@ -4,3 +4,16 @@ modoboa-alias-pipe
 Modoboa administration panel extension for manage alias pipe commands.
 
 Status : pre-version, not complete, not stable, don't use in production.
+
+
+Postfix configuration
+---------------------
+
+After ``postfix_maps`` execution :
+
+    $ modoboa-admin.py postfix_maps --dburl mysql.... --extensions ... modoboa-alias-pipe
+
+Append this configuration in ``/etc/postfix/main.cf`` :
+
+    alias_maps = hash:/etc/aliases mysql:/etc/postfix/mysql/sql-aliases-pipe.cf ...
+    virtual_alias_maps = mysql:/etc/postfix/mysql/sql-virtual-aliases-pipe.cf ...
---
+++ modoboa_alias_pipe/postfix_maps.py
@@ -0,0 +1,17 @@
+"""Map file definitions for postfix."""
+
+from modoboa.core.commands.postfix_maps import registry
+
+
+class VirtualAliasesPipeMap(object):
+    filename = 'sql-virtual-aliases-pipe.cf'
+
+    mysql = "SELECT concat(ap.address, '-', dom.name, '-pipe') FROM alias_pipe ap INNER JOIN admin_domain dom ON ap.domain_id=dom.id WHERE dom.name='%d' AND dom.enabled=1 AND ap.address='%u' AND ap.enabled=1 LIMIT 1"  # NOQA
+
+
+class AliasesPipeMap(object):
+    filename = 'sql-aliases-pipe.cf'
+
+    mysql = "SELECT ap.command FROM alias_pipe ap INNER JOIN admin_domain dom ON ap.domain_id=dom.id WHERE concat(ap.address, '-', dom.name, '-pipe') = '%s' LIMIT 1"  # NOQA
+
+registry.add_files([VirtualAliasesPipeMap, AliasesPipeMap])
