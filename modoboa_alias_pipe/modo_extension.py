f062081adeffc21e3552e4c71af77708b764b7dd
Gael Pasgrimaud <gael@gawel.org>
2017-03-21 11:04:38 +0100

    1.7.x compat

--- modoboa_alias_pipe/modo_extension.py
+++ modoboa_alias_pipe/modo_extension.py
@@ -3,15 +3,15 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _, ugettext_lazy
 
 from modoboa.core.extensions import ModoExtension, exts_pool
-from modoboa.lib import events
+from modoboa.core import signals as core_signals
 from modoboa.admin import signals
 from modoboa.lib.email_utils import split_mailbox
 from .models import AliasPipe
 
 
-@events.observe("AdminMenuDisplay")
-def admin_menu(target, user):
-    if target != "top_menu":
+@receiver(core_signals.extra_admin_menu_entries)
+def admin_menu(sender, location, user, **kwargs):
+    if location != "top_menu":
         return []
 
     entries = []
